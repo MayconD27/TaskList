4920d800e02485c9b5ddd4b78c68163441c4ce7c
MayconD27 <mdsx627@gmail.com>
2025-10-22 20:56:09 -0300

    restaurando backend

---
+++ backend/database/migrations/2025_10_22_000004_create_tarefas_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void {
+        Schema::create('tarefas', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo', 50);
+            $table->enum('status', ['feito', 'fazendo', 'pendente', 'em_progresso', 'concluido']);
+            $table->text('comentario')->nullable();
+            $table->foreignId('usuario_id') ->nullable()->constrained('usuarios')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId('setor_id')->nullable()->constrained('setores')->nullOnDelete()->cascadeOnUpdate();
+            $table->foreignId('projeto_id')->nullable()->constrained('projetos')->nullOnDelete()->cascadeOnUpdate();
+            $table->date('data_criacao')->nullable();
+            $table->integer('delete_flag')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void {
+        Schema::dropIfExists('tarefas');
+    }
+};
